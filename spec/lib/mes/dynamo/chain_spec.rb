ab08eec7f0321964a47cfb666115450e529696e6
Roman Lupiychuk <luckyjazzbo@gmail.com>
2016-06-13 12:56:53 +0300

    MES-343: LTE Core DynamoDB: Fetch documents in batches

--- spec/lib/mes/dynamo/chain_spec.rb
+++ spec/lib/mes/dynamo/chain_spec.rb
@@ -37,110 +37,166 @@ RSpec.describe Mes::Dynamo::Chain do
   let(:avatar_title) { 'Avatar' }
   let(:superman_title) { 'Superman' }
 
-  let(:chain_with_where) do
-    subject
-      .index('title_index')
-      .where(title: avatar_title)
-  end
+  context 'when scaning' do
+    subject { described_class.new(Movie, scan: true) }
 
-  subject { described_class.new(Movie) }
-
-  describe '#index' do
-    it 'updates query index' do
-      expect(subject.index('test').index_name).to eq 'test'
-    end
-  end
-
-  describe '#limit' do
-    it 'updates query limit' do
-      expect(subject.limit(123).limit_of_results).to eq 123
-    end
-  end
-
-  describe '#where' do
-    context 'when table is empty' do
-      it 'yeilds nothing' do
-        expect { |block|
-          chain_with_where.each(&block)
-        }.not_to yield_control
-      end
-    end
-
-    context 'when there is an item' do
+    context '#each' do
       before do
         create_movie(:avatar)
         create_movie(:superman)
       end
 
-      it 'yeilds all items' do
+      it 'returns all records' do
+        expect { |block| subject.each(&block) }.to yield_control.twice
+      end
+
+      it 'filters records' do
         expect { |block|
-          chain_with_where.each(&block)
+          subject.where(title: avatar_title).each(&block)
         }.to yield_control.once
       end
-
-      it 'excepts expression along with values' do
-        expect(
-          subject
-            .index('title_index')
-            .where('title = :title', title: avatar_title)
-            .to_a
-            .size
-        ).to eq 1
-      end
     end
-  end
 
-  describe '#first' do
-    context 'when table is empty' do
-      it 'returns nil' do
-        expect(chain_with_where.first).to be nil
+    context '#limit' do
+      before do
+        create_movie(:avatar)
+        create_movie(:superman)
+      end
+
+      it 'limits results' do
+        expect { |block| subject.limit(1).each(&block) }.to yield_control.once
       end
     end
 
-    context 'when there is an item' do
+    context '#first' do
       before do
         create_movie(:avatar)
       end
 
-      context 'without where condition' do
-        it 'returns last item' do
-          expect {
-            subject.first.title
-          }.to raise_error Mes::Dynamo::InvalidQuery
-        end
+      it 'returns the first object' do
+        expect(subject.first.title).to eq avatar_title
       end
+    end
 
-      context 'with where condition' do
-        it 'returns last item' do
-          expect(chain_with_where.first.title).to eq avatar_title
-        end
+    context '#last' do
+      it 'raises error' do
+        expect { subject.last }.to raise_error Mes::Dynamo::InvalidQuery
       end
     end
   end
 
-  describe '#last' do
-    context 'when table is empty' do
-      it 'returns nil' do
-        expect(chain_with_where.last).to be nil
+  context 'when querying' do
+    let(:chain_with_where) do
+      subject
+        .index('title_index')
+        .where(title: avatar_title)
+    end
+
+    subject { described_class.new(Movie) }
+
+    describe '#index' do
+      it 'updates query index' do
+        expect(subject.index('test').index_name).to eq 'test'
       end
     end
 
-    context 'when there is an item' do
+    describe '#limit' do
       before do
         create_movie(:avatar)
+        create_movie(:avatar)
+        create_movie(:superman)
       end
 
-      context 'without where condition' do
-        it 'returns last item' do
-          expect {
-            subject.last.title
-          }.to raise_error Mes::Dynamo::InvalidQuery
+      it 'limits results' do
+        expect { |block| chain_with_where.limit(1).each(&block) }.to yield_control.once
+      end
+    end
+
+    describe '#where' do
+      context 'when table is empty' do
+        it 'yeilds nothing' do
+          expect { |block|
+            chain_with_where.each(&block)
+          }.not_to yield_control
         end
       end
 
-      context 'with where condition' do
-        it 'returns last item' do
-          expect(chain_with_where.last.title).to eq avatar_title
+      context 'when there is an item' do
+        before do
+          create_movie(:avatar)
+          create_movie(:superman)
+        end
+
+        it 'yeilds all items' do
+          expect { |block|
+            chain_with_where.each(&block)
+          }.to yield_control.once
+        end
+
+        it 'excepts expression along with values' do
+          expect(
+            subject
+              .index('title_index')
+              .where('title = :title', title: avatar_title)
+              .to_a
+              .size
+          ).to eq 1
+        end
+      end
+    end
+
+    describe '#first' do
+      context 'when table is empty' do
+        it 'returns nil' do
+          expect(chain_with_where.first).to be nil
+        end
+      end
+
+      context 'when there is an item' do
+        before do
+          create_movie(:avatar)
+        end
+
+        context 'without where condition' do
+          it 'returns last item' do
+            expect {
+              subject.first.title
+            }.to raise_error Mes::Dynamo::InvalidQuery
+          end
+        end
+
+        context 'with where condition' do
+          it 'returns last item' do
+            expect(chain_with_where.first.title).to eq avatar_title
+          end
+        end
+      end
+    end
+
+    describe '#last' do
+      context 'when table is empty' do
+        it 'returns nil' do
+          expect(chain_with_where.last).to be nil
+        end
+      end
+
+      context 'when there is an item' do
+        before do
+          create_movie(:avatar)
+        end
+
+        context 'without where condition' do
+          it 'returns last item' do
+            expect {
+              subject.last.title
+            }.to raise_error Mes::Dynamo::InvalidQuery
+          end
+        end
+
+        context 'with where condition' do
+          it 'returns last item' do
+            expect(chain_with_where.last.title).to eq avatar_title
+          end
         end
       end
     end
