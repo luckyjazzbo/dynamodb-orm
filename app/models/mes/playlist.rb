24564105d1963a4b431e1f43dfe66b71228a8b29
Anton Priadko <antonpriadko@gmail.com>
2016-07-04 14:18:59 +0300

    Added field query, tenant_id_index

--- app/models/mes/playlist.rb
+++ app/models/mes/playlist.rb
@@ -4,6 +4,9 @@ module Mes
 
     table name: "mes-playlists-#{RACK_ENV}", primary_key: :uuid
     field :tenant_id, type: :string
+    field :query, type: :map
+
+    table_index :tenant_id, name: 'tenant_id_index'
 
     before_create do
       self.uuid ||= SecureRandom.uuid
