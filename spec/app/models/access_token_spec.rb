e81b345c58fa0b218db3a07516a700575a824c2d
Oleg Keene <ol.keene@gmail.com>
2016-07-04 15:55:54 +0300

    [MES-574] New attributes for AccessToken

--- spec/app/models/access_token_spec.rb
+++ spec/app/models/access_token_spec.rb
@@ -1,23 +1,37 @@
 require 'spec_helper'
 
 RSpec.describe Mes::AccessToken do
-  describe '#access_token' do
-    subject { described_class.new }
-
-    it 'is generated automaticaly' do
-      expect { subject.save }
-        .to change { subject.access_token }
-        .from(nil)
+  context 'attributes' do
+    describe 'default values' do
+      {
+        active: true,
+        type: 'EMBED',
+        device_class: 'BROWSER'
+      }.each do |attr, value|
+        it "sets default value for #{attr}" do
+          expect(subject.send(attr)).to eq(value)
+        end
+      end
     end
 
-    it 'is not replacing the manualy set token' do
-      subject.access_token = 'some_token'
-      expect { subject.save }.not_to change { subject.access_token }
-    end
+    describe '#access_token' do
+      subject { described_class.new }
 
-    it 'is generates 32-char string' do
-      subject.save
-      expect(subject.access_token.size).to eq 32
+      it 'is generated automaticaly' do
+        expect { subject.save }
+          .to change { subject.access_token }
+          .from(nil)
+      end
+
+      it 'is not replacing the manualy set token' do
+        subject.access_token = 'some_token'
+        expect { subject.save }.not_to change { subject.access_token }
+      end
+
+      it 'is generates 32-char string' do
+        subject.save
+        expect(subject.access_token.size).to eq 32
+      end
     end
   end
 
