84696c89e72d0bded73132718c5f00ba04e7fb2b
Roman Lupiychuk <luckyjazzbo@gmail.com>
2016-08-18 17:57:20 +0300

    Improve factory of OriginalResource

--- spec/factories/mes/original_resource.rb
+++ spec/factories/mes/original_resource.rb
@@ -10,7 +10,6 @@ FactoryGirl.define do
         'asset_type'  => 'video',
         'version'     => version,
         'tenant_id'   => "t-#{SecureRandom.base64}",
-        'status'      => 'READY',
         'language'    => 'en',
         'copyright'   => 'This is mine',
         'duration'    => 1111.11,
@@ -21,7 +20,60 @@ FactoryGirl.define do
           'CARA' => 'PG13'
         },
         'start_date_absolute' => '2016-07-28 10:23:54+00',
-        'end_date_absolute'   => '2016-07-28 10:23:54+00'
+        'end_date_absolute'   => '2016-07-28 10:23:54+00',
+        'status'              => 'READY'
+      }
+    end
+
+    children_data do
+      {
+        'images' => [
+          { 'id' => 'i-321', 'url' => 'http://url' }
+        ],
+
+        'taxonomies' => [
+          { 'id' => 'tx-123' }
+        ],
+
+        'ad_profiles' => [
+          {
+            'ad_groups' => [
+              {
+                'geolocation' => 'en',
+                'sales_house_id' => 't-tratatatata',
+                'reach_measured' => { 'postrolls' => ['begin-[__VIDEOID__]-end'] }
+              }
+            ]
+          }
+        ],
+
+        'license_profiles' => [
+          {
+            'asset_type'          => 'license_profile',
+            'type'                => 'SYNDICATION',
+            'id'                  => 'l-3d92dh9283hd23',
+            'tenant_id'           => 't-123113233',
+            'name'                => 'my cool License Profile for GNTM',
+            'name_short'          => 'myrule',
+            'start_date'          => 1352345234,
+            'end_date'            => 1352345234,
+            'products'            => ['FIXED_PRICE', 'ADS'],
+            'sales_houses'        => ['t-123113233'],
+            'geo_locations'       => ['DE', 'AT'],
+            'device_classes'      => ['BROWSER', 'MOBILE', 'SETTOPBOX', 'SMARTTV', 'HBBTV', 'GAMECONSOLE', 'HDMISTICK'],
+            'bandwidth_max'       => { 'BROWSER' => 400, 'MOBILE' => 100 },
+            'publisher_whitelist' => ['t-w123efwe', 't-asde345ad'],
+            'publisher_blacklist' => ['t-ef23f23f', 't-ef23f232'],
+            'max_resolution'      => 'SD',
+            'drm'                 => false,
+            'entitlement'         => false,
+            'created_at'          => 123123123,
+            'modified_at'         => 1234234212,
+            'version'             => 2,
+            'status'              => 'READY',
+            'player_logo'         => 'i-1234'
+          }
+        ]
       }
     end
   end
