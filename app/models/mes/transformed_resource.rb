460205f6cb235ba8aa7dbb9212b4d10d3a513696
Roman Lupiychuk <luckyjazzbo@gmail.com>
2016-08-18 18:28:57 +0300

    Add uuid to TransformedResource; remove TransformationStep

--- app/models/mes/transformed_resource.rb
+++ app/models/mes/transformed_resource.rb
@@ -3,12 +3,19 @@ module Mes
     include ::Mes::Dynamo::Timestamps
 
     table name: "lte-transformed-resources-#{RACK_ENV}",
-          primary_key: :content_id
+          primary_key: :uuid
 
+    field :content_id, type: :string
     field :data, default: -> { {} }
 
     belongs_to :original_resource, class_name: 'Mes::OriginalResource',
                                    foreign_key: :original_resource_uuid
+
+    table_index :content_id, name: 'content_id_at_index'
+
+    before_create do
+      self.uuid ||= SecureRandom.uuid
+    end
 
     validates :content_id,             presence: true
     validates :original_resource_uuid, presence: true
