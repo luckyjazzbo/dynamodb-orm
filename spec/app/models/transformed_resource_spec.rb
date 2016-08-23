2ea4bb85820ea0c0925143df813a41df786f3115
Bohdan Hryshchenko <bgr11n@gmail.com>
2016-08-23 14:48:25 +0300

    asset_type as field + specs
    asset_type as field not in data
    
    updated spec and factory
    
    Added validation to asset_type field + specs

--- spec/app/models/transformed_resource_spec.rb
+++ spec/app/models/transformed_resource_spec.rb
@@ -3,22 +3,6 @@ require 'spec_helper'
 RSpec.describe Mes::TransformedResource do
   include_context 'with mes tables'
 
-  describe '#asset_type' do
-    context 'by default' do
-      it 'expected to be nil' do
-        expect(subject.asset_type).to be_nil
-      end
-    end
-
-    context 'when present' do
-      subject { described_class.new(data: { 'asset_type' => 'video' }) }
-
-      it 'is not nil' do
-        expect(subject.asset_type).to eq('video')
-      end
-    end
-  end
-
   describe '.create_from_original_resource!' do
     let(:original_resource) { FactoryGirl.create(:original_resource) }
     let(:data) { { 'test_field' => 'test data' } }
@@ -30,7 +14,8 @@ RSpec.describe Mes::TransformedResource do
     end
 
     it 'creates valid object from original_resource' do
-      expect(subject.attributes.slice('content_id', 'original_resource_uuid', 'data')).to eq(
+      expect(subject.attributes.except('uuid', 'created_at', 'updated_at')).to eq(
+        'asset_type'             => original_resource.data['asset_type'],
         'content_id'             => original_resource.content_id,
         'original_resource_uuid' => original_resource.uuid,
         'data'                   => data
--- spec/factories/mes/transformed_resource.rb
+++ spec/factories/mes/transformed_resource.rb
@@ -4,11 +4,11 @@ FactoryGirl.define do
       original_resource { create(:original_resource) }
     end
 
+    asset_type             { original_resource.data['asset_type'] }
     content_id             { original_resource.content_id }
     original_resource_uuid { original_resource.uuid }
     data do
       {
-        'asset_type' => 'video',
         'modified_at' => 123343252.0,
         'clip_duration' => 12312.0,
         'image' => { 'url' => 'http://image.example.com/image' },
