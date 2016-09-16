02c6e40b62aef7c762ecd1aac39be61ef918d4d6
Bohdan Hryshchenko <bgr11n@gmail.com>
2016-09-16 15:59:32 +0300

    Change type -> tenant_type, created index by tenant type

--- spec/factories/mes/contact_request.rb
+++ spec/factories/mes/contact_request.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :contact_request, class: 'Mes::ContactRequest' do
-    email   'email@example.com'
-    type    'publisher'
-    domain  'example.com'
-    name    { SecureRandom.uuid }
-    message { SecureRandom.uuid }
+    email        'email@example.com'
+    tenant_type  'publisher'
+    domain       'example.com'
+    name         { SecureRandom.uuid }
+    message      { SecureRandom.uuid }
   end
 end
